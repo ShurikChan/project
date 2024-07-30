e2568587c2fdbc3e00fd2372da0fc81b8b7bb604
Shurik <156313194+ShurikChan@users.noreply.github.com>
2024-07-30 14:29:53 +0300

    added PATCH partial_update only to objects with new status

--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .models import Pereval
 from .serializers import *
-from .filters import OnlyNewPerevalFilter
+from django.http import HttpResponse
 # from rest_framework.generics import UpdateAPIView
 # Create your views here.
 
@@ -21,8 +21,12 @@ class LevelViewSet(viewsets.ModelViewSet):
     serializer_class = LevelSerializer
 
 
-class PerevalViewSet(viewsets.ReadOnlyModelViewSet):
+class PerevalViewSet(viewsets.ModelViewSet):
     queryset = Pereval.objects.all()
     serializer_class = PerevalSerializer
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        return super().partial_update(request, *args, **kwargs) if instance.status =='new' else HttpResponse('Cannot update a completed pereval', status=status.HTTP_403_FORBIDDEN)
 
 
